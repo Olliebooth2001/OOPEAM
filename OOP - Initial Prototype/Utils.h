41cab46fba36e8226c7bc90e3c4be7acea770c93
rslvhsg <68112227+rslvhsg@users.noreply.github.com>
2021-04-21 11:20:54 +0100

    added file data processing functions in utils

--- OOP - Initial Prototype/Utils.h
+++ OOP - Initial Prototype/Utils.h
@@ -12,6 +12,9 @@ class Utils
     void savefile();
     static bool StartsWith(std::string fullText, std::string searchText);
 
+    List<Game> ReadGameDataFromFile(const std::string& directory);
+    List<Account*> ProcessFileData(Application& app, const std::string& dir);
+
     // TODO: 2 versions of ToUpper
     std::string fullText;
     std::string searchText;
